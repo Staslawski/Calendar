2f8930d8bf4b4d4a899ef24cbf85fb446b0b6a4c
Staslawski <bumazhkine@bk.ru>
2021-10-28 21:54:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+const date = new Date();
+// console.log(date);
+
+const renderCalendar = () => {
+    date.setDate(1);
+    // console.log(date.getDay());
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    const lastDay = new Date(date.getFullYear(), date.getMonth() +1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = date.getDay() - 1;
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1,0).getDay();
+    const monthDays = document.querySelector('.days');
+    const nextDays = 7 - lastDayIndex;
+
+    let month = document.querySelector('.date h1');
+    let string = document.querySelector('.date p');
+    let days = "";
+
+    month.innerHTML = months[date.getMonth()];
+    string.innerHTML = new Date().toDateString();
+    console.log(string);
+
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+    }
+    monthDays.innerHTML = days;
+}
+
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+
+    renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+
+    renderCalendar();
+});
+
+renderCalendar();
+
+
+
+
+
